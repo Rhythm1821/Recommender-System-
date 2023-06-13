d9e8eba6dac2b9dfd2efc7f2df86e74184c88ca6
Rhythm1821 <alamrawat1821@gmail.com>
2023-06-13 13:42:24 +0530

    send all

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pickle as pkl
+import pandas as pd
+
+movies_dict = pkl.load(open('movie_dict.pkl','rb'))
+movies = pd.DataFrame(movies_dict)
+
+similarity = pkl.load(open('similarity.pkl','rb'))
+
+def recommend(movie):
+    movie_index = movies[movies['title']==movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
+
+    recommended_movies = []
+    for i in movies_list:
+        recommended_movies.append(movies.iloc[i[0]].title)
+    return recommended_movies
+
+
+st.title('Movie recommender system')
+
+selected_movie_name=st.selectbox(
+'Select a movie', 
+movies['title'].values
+)
+
+if st.button('Recommend'):
+    recommendations = recommend(selected_movie_name)
+    for i in recommendations:
+        st.write(i)
